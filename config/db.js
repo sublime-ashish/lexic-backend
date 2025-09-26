9acb9349e6a6c9a34e22478dd15903143e18c417
sutariya,ashish34 <sutariyaashish34@gmail.com>
2025-09-26 21:11:04 +0530

    db changes

--- config/db.js
+++ config/db.js
@@ -6,8 +6,18 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "rbac_db",
+  database: process.env.DB_NAME || "railway",
 });
+
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
 
 db.connect((err) => {
   if (err) throw err;
